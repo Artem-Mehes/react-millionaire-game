a2af1be6cff2b2b4176f21bc753ffdb084965984
Artem Meges <artemmeges@gmail.com>
2020-10-15 17:47:02 +0300

    Updates

---
+++ precache-manifest.fdd2567c0abf1fbc14b5e25dcead9941.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e63c6958f1de07236ad4636ce1bd5cd5",
+    "url": "/react-millionaire-game/index.html"
+  },
+  {
+    "revision": "a7352aabaaa74345dbcc",
+    "url": "/react-millionaire-game/static/css/main.527487a7.chunk.css"
+  },
+  {
+    "revision": "eb027d0c4771f7e84352",
+    "url": "/react-millionaire-game/static/js/2.3df6a719.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-millionaire-game/static/js/2.3df6a719.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7352aabaaa74345dbcc",
+    "url": "/react-millionaire-game/static/js/main.a3004f64.chunk.js"
+  },
+  {
+    "revision": "85937f01b98b7d9ff37b",
+    "url": "/react-millionaire-game/static/js/runtime-main.c1ae4a9f.js"
+  },
+  {
+    "revision": "0f121bb37307a1dd9e0e785f4b245a57",
+    "url": "/react-millionaire-game/static/media/game-start-bg.0f121bb3.png"
+  },
+  {
+    "revision": "86b8c05e20452bf82418941280836ad5",
+    "url": "/react-millionaire-game/static/media/hand.86b8c05e.svg"
+  }
+]);
